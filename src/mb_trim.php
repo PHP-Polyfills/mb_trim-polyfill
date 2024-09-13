1f873efc39f9ca0c648c349a7b22e8c7986d57f2
Aleksey Kuznietsov <utilmind@gmail.com>
2024-09-13 12:33:56 -0400

    Comment on UTF8_SPACES

--- src/mb_trim.php
+++ src/mb_trim.php
@@ -1,4 +1,6 @@
 <?php
+// UTF8_SPACES specifies standard space and line break characters in the UTF-8 character encoding standard.
+// Use this constant to add more characters to trim from the string together with spaces.
 define('UTF8_SPACES', " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}");
 
 /**
